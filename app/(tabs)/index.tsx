528ca896aeee688b1a769cd1840f321248655f53
morachake <jacobmoracha@gmail.com>
2024-07-03 16:58:58 +0300

    creating a header with filter for the mainn app

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,14 @@
+import Calendar from '@/components/home/Calendar';
+import NavBar from '@/components/home/NavBar';
 import { Image, StyleSheet, Platform, Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function HomeScreen() {
   return (
     <SafeAreaView>
-      <Text>Home</Text>
+      
+      <NavBar/>
+      <Calendar/>
     </SafeAreaView>
   );
 }
---
+++ components/home/NavBar.tsx
@@ -0,0 +1,59 @@
+import { View ,Text, StyleSheet,TouchableOpacity} from "react-native";
+import { AntDesign } from '@expo/vector-icons';
+export default function NavBar() {
+    return (
+        <View style={styles.container}>
+            <Text>Journal</Text>
+            <View style={styles.cards}>
+                <TouchableOpacity style={styles.card}>
+                    <Text>Day</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.card}>
+                    <Text>Week</Text>
+                </TouchableOpacity>
+               
+            </View>
+            <View style={styles.filter}>
+                <AntDesign name="filter" size={24} color="black" />
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor:'#FF5987',
+        padding: 16,
+        flexDirection: 'row',
+        justifyContent:'space-between',
+        alignItems: 'center',
+        height: 56,
+    },
+    cards:{
+        width: 150,
+        padding: 5,
+        flexDirection: 'row',
+        gap: 3,
+        borderRadius: 8,
+        backgroundColor:'#FFFFFF'
+
+    },
+    card: {
+        width: 50,
+        height: 30,
+        backgroundColor: 'white',
+        borderWidth: 1,
+        borderColor:"#FF5987",
+        borderRadius: 12,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    filter: {
+        width: 40,
+        height: 40,
+        backgroundColor: 'white',
+        borderRadius: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+})
